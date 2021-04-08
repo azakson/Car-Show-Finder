dd9e528d4fffc5c6f4aba173d5b39a437a618bfe
Avery Zakson <avery.zakson@gmail.com>
2021-04-08 10:33:56 -0500

    Initial Commit

---
+++ Car Show Finder/Car_Show_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_Show_FinderApp.swift
+//  Car Show Finder
+//
+//  Created by Avery Zakson on 4/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Car_Show_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Car Show FinderTests/Car_Show_FinderTests.swift
@@ -0,0 +1,33 @@
+//
+//  Car_Show_FinderTests.swift
+//  Car Show FinderTests
+//
+//  Created by Avery Zakson on 4/8/21.
+//
+
+import XCTest
+@testable import Car_Show_Finder
+
+class Car_Show_FinderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
